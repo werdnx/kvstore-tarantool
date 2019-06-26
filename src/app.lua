03c1b96c4e526fd22f50baace45a9b11de02f199
dmitrenko <werdnx@gmail.com>
2019-06-26 18:15:17 +0300

    POST, GET, PUT, DELETE according REST API spec

--- src/app.lua
+++ src/app.lua
@@ -2,35 +2,68 @@ local dao = require('db')
 local log = require('log')
 local httpd = require('http.server')
 local SERVER_IP = os.getenv('SERVER_IP')
+local json = require('json')
 
 function put(key, value)
     dao:put(key, value)
-    return {'OK'}
 end
 
 function get(key)
-    return {dao:get(key)}
+    return dao:get(key)
 end
 
 function delete(key)
-    deleted = dao:delete(key)
-    return {deleted}
+    return dao:delete(key)
+end
+
+function badResponse(req, status, message)
+    local resp = req:render({ text = message })
+    resp.status = status
+    return resp
+end
+
+local function deleteHandler(req)
+    local obj = get(req:stash('key'))
+    if obj == nil then
+        return badResponse(req, 404, 'Key ' .. req:stash('key') .. ' not found')
+    else
+        return req:render({text = delete(req:stash('key'))})
+    end
 end
 
 local function postHandler(req)
-    log.info('Recieved POST')
-    if req:post_param('method') == 'get' then
-        return get(req:post_param('key'))
-    elseif req:post_param('method') == 'put' then
-        return put(req:post_param('key'),req:post_param('value'))
-    elseif req:post_param('method') == 'delete' then
-        return delete(req:post_param('key'))
+    if req:stash('key') == nil or req:stash('value') == nil then
+        return badResponse(req, 400, 'Bad params key or value is null')
+    else
+        local obj = get(req:stash('key'))
+        if #obj == 0 then
+            return badResponse(req, 404, 'Key ' .. req:stash('key') .. ' not found')
+        else
+            put(req:stash('key'), req:stash('value'))
+            return req:render({text = ''})
+        end
+    end
+end
+
+local function getHandler(req)
+    return req:render({ text = json.encode(get(req:stash('key'))) })
+end
+
+local function putHandler(req)
+    if req:stash('key') == nil or req:stash('value') == nil then
+        return badResponse(req, 400, 'Bad params key or value is null')
+    else
+        put(req:stash('key'), req:stash('value'))
+        return req:render({text = ''})
     end
 end
 
 function main()
     local server = httpd.new(SERVER_IP, 8080)
-    server:route({ path = '/kv'}, postHandler)
+    server:route({ path = '/kv/:key/:value', method = 'POST' }, postHandler)
+    server:route({ path = '/kv/:key/:value', method = 'PUT' }, putHandler)
+    server:route({ path = '/kv/:key', method = 'GET' }, getHandler)
+    server:route({ path = '/kv/:key', method = 'DELETE' }, deleteHandler)
     server:start()
 end
 
